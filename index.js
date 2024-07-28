436c98ec9efff142e1743e56cb0b87d475871309
kujieun <115772471+kujieun@users.noreply.github.com>
2024-07-28 14:16:50 +0900

    지도에 경로 표시

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
+/* 기존
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+*/
+
+
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+//import bus from './scripts/bus';
+//import PathAndMap from './scripts/PathAndMap';
+import Parking from './scripts/Parking';
+
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => PathAndMap);
