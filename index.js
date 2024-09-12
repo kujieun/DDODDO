eb70061e710276fe1d6bf0e0180864ed822da2e6
kujieun <115772471+kujieun@users.noreply.github.com>
2024-09-12 17:19:14 +0900

    index.js 정리

--- index.js
+++ index.js
@@ -15,18 +15,22 @@ AppRegistry.registerComponent(appName, () => App);
 import {AppRegistry} from 'react-native';
 import App from './App';
 import SignupHome from './scripts/SignupHome';
-//import PathAndMap from './scripts/PathAndMap';
-import testtest from './scripts/testtest';
-//import FindRoute from './scripts/FindRoute';
-import gangneungmap from './scripts/gangneungmap'; //장소 카테고리 지도에 표시
+import LoginPage from './scripts/LoginPage'; // 로그인 기능 구현
 import SignupTerm from './scripts/SignupTerm';
 import SignupNickname from './scripts/SignupNickname';
-import RestaurantHome from './scripts/RestaurantHome'; // 추천 맛집
-import TourPlaceHome from './scripts/TourPlaceHome'; // 추천 여행지
-import LoginPage from './scripts/LoginPage'; // 추천 여행지
+/* ---------------------------------------------------------------------------- */
 import MainHome from './scripts/MainHome'; // 메인 홈
 
+import testtest from './scripts/testtest'; // 강릉 지도 테스트 버전
+import gangneungmap from './scripts/gangneungmap'; // 강릉 지도
+import RestaurantHome from './scripts/RestaurantHome'; // 추천 맛집
+import TourPlaceHome from './scripts/TourPlaceHome'; // 추천 여행지
+
+
+
 import Detail from './scripts/Detail';
+
+/* ---------------------------------------------------------------------------- */
 
 import Community from './screens/Community';
 import Weather from './screens/Weather'
