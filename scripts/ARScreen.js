5642c99de80d5ae4180123ea86daf2a5eae0997d
kujieun <115772471+kujieun@users.noreply.github.com>
2024-09-16 17:34:53 +0900

    AR 성공

--- scripts/ARScreen.js
+++ scripts/ARScreen.js
@@ -1,26 +1,56 @@
-import { ArViewerView } from 'react-native-ar-viewer';
-import { Platform, View } from 'react-native';
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroText,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 
-const App = () => {
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState("Initializing AR...");
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("onInitialized", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("Hello World!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <ArViewerView
-        style={{ flex: 1 }}
-        model={Platform.OS === 'android' ? 'dice.glb' : 'dice.usdz'}
-        lightEstimation
-        manageDepth
-        allowRotate
-        allowScale
-        allowTranslate
-        disableInstantPlacement
-        onStarted={() => console.log('started')}
-        onEnded={() => console.log('ended')}
-        onModelPlaced={() => console.log('model displayed')}
-        onModelRemoved={() => console.log('model not visible anymore')}
-        planeOrientation="both"
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
       />
-    </View>
+    </ViroARScene>
   );
 };
 
-export default App;
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
