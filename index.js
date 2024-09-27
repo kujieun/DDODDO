ef8856428f4a71526d46f7b64e5ad4fa73a9cf66
ssukim267 <81361709+ssukim267@users.noreply.github.com>
2024-09-27 16:34:02 +0900

    변경

--- index.js
+++ index.js
@@ -67,4 +67,4 @@ import Tip from './screens/Tip'
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => TestNavigation);
+AppRegistry.registerComponent(appName, () => App);
